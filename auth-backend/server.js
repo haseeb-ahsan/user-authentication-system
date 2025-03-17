f3653678d3d2f2855d25f5b381e3943087c5cba2
Haseeb Ahsan Tahir <84566594+haseeb-ahsan@users.noreply.github.com>
2025-03-17 13:08:11 +0000

    Update server.js
    changes to check the build

--- auth-backend/server.js
+++ auth-backend/server.js
@@ -10,13 +10,7 @@ dotenv.config();
 const app = express();
 app.use(cors());
 
-mongoose.connect(process.env.MONGO, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  ssl: true,
-  sslValidate: true,
-  sslCA: undefined,
-});
+mongoose.connect(process.env.MONGO);
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'Connection error:'));
